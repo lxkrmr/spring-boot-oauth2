13976f7b2f83a9ddacb2a45f49658f5db8c14f93
Alex <lxkrmr@icloud.com>
2022-04-07 20:06:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Boot oAuth2
+
+Finally, some time to follow along the spring tutorial **Spring Boot ans OAuth2"
+
+https://spring.io/guides/tutorials/spring-boot-oauth2/
---
+++ src/main/java/bootoauth/BootOauthApplication.java
@@ -0,0 +1,13 @@
+package bootoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOauthApplication.class, args);
+	}
+
+}
